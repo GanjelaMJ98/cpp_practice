0bbd8a9f8dc234b2d8e0244d79edbb52713843ce
pavel <ganjela98@gmail.com>
2019-05-14 18:21:10 +0300

    Added class_person part2
    Signed-off-by: pavel <ganjela98@gmail.com>

--- week_3/class_person.cpp
+++ week_3/class_person.cpp
@@ -40,6 +40,32 @@ public:
 
 		return ans;
 	}
+
+	string GetFullNameWithHistory(int year){
+		string ans = "";
+
+		string f_name = GetName(year, map_first_name);
+		string l_name = GetName(year, map_last_name);
+
+		if(f_name == "" && l_name ==""){
+			ans = "Incognito";
+		}
+
+		if(f_name != "" && l_name ==""){
+			ans = f_name + " " + GetPastName(year, f_name,map_first_name) + " with unknown last name";
+		}
+
+		if(f_name == "" && l_name !=""){
+			ans = l_name + " " + GetPastName(year, l_name,map_last_name) + " with unknown first name";
+		}
+
+		if(f_name != "" && l_name !=""){
+			ans = f_name + " " + GetPastName(year, f_name,map_first_name) + " " + l_name + " " + GetPastName(year, l_name,map_last_name);
+		}
+
+		return ans;
+
+	}
 private:
 	map<int, string> map_first_name;
 	map<int, string> map_last_name;
@@ -54,6 +80,23 @@ private:
 			}
 		}
 
+		return ans;
+	}
+
+	string GetPastName(const int& year, const string& current_name, const map<int , string>& names){
+		string ans = " ";
+		for(auto& name : names){
+			if(name.first <= year){
+				if(name.second != current_name){
+					ans += name.second + " ";
+				}
+			}else{
+				break;
+			}
+		}
+		if(ans != " "){
+			ans = "(" + ans + ")";
+		}
 		return ans;
 	}
 };
@@ -76,6 +119,23 @@ int main(){
 	for(int year : {1969,1970}){
 		cout << person.GetFullName(year) << endl;
 	}
+	cout << endl;
+	
+	//part 2
+	person.ChangeFirstName(1990, "Polina");
+	person.ChangeLastName(1990, "Volkova-Sergeeva");
+	cout << person.GetFullNameWithHistory(1990) << endl;
+
+	person.ChangeFirstName(1966, "Pauline");
+	cout << person.GetFullNameWithHistory(1966) << endl;
+
+	person.ChangeLastName(1960, "Sergeeva");
+	for(int year: {1960,1967}){
+		cout << person.GetFullNameWithHistory(year) << endl;
+	}
+
+	person.ChangeLastName(1961, "Ivanova");
+	cout << person.GetFullNameWithHistory(1967) << endl;
 
 	return 0;
 }
